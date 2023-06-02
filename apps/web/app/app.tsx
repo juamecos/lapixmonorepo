014367b9a4ab863093862f37f06c8da1add01a5c
Juan José Mena Costa <juamecos@gmail.com>
2023-06-02 05:18:06 +0200

    removing secrets

---
+++ apps/web/app/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from './page';
+
+
+function MyApp() {
+  return <Page />
+}
+
+export default MyApp;
---
+++ apps/web/pages/home-screen/index.tsx
@@ -0,0 +1,14 @@
+import styles from './index.module.scss';
+
+/* eslint-disable-next-line */
+export interface HomeScreenProps {}
+
+export function HomeScreen(props: HomeScreenProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to HomeScreen!</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
